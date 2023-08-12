f6f4197be576db849ed1a999fb5820a6171fce36
Shuezi <73438401+Shuezi@users.noreply.github.com>
2023-08-12 12:21:24 -0300

    Commit iniciañ

---
+++ ClickOnceDeploTest/ClickOnceDeploTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickOnceDeploTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (ApplicationDeployment.IsNetworkDeployed)
+                Console.WriteLine(ApplicationDeployment.CurrentDeployment.CurrentVersion);
+            else
+                Console.WriteLine("Sin deployment");
+
+            Console.ReadLine();
+
+        }
+    }
+}
